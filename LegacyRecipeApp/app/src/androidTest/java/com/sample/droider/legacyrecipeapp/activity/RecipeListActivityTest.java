2d1c7a17680c8f6237aed442f0e450597f9dbae2
okamotoatsushi <okamotoatsushi@waku-2.com>
2020-03-03 11:06:34 +0900

    Espressoテストコード追加

---
+++ LegacyRecipeApp/app/src/androidTest/java/com/sample/droider/legacyrecipeapp/activity/RecipeListActivityTest.java
@@ -0,0 +1,58 @@
+package com.sample.droider.legacyrecipeapp.activity;
+
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import com.sample.droider.legacyrecipeapp.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecipeListActivityTest {
+    @Rule
+    public ActivityTestRule<RecipeListActivity> mActivityTestRule = new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void labelTest() {
+        onView(withId(R.id.recycler))
+                .perform(RecyclerViewActions.scrollToPosition(0))
+                .check(matches(atPositionOnView(0,
+                        withText("おすすめ"), R.id.text_recommend)));
+    }
+
+    public static Matcher<View> atPositionOnView(final int position, final Matcher<View> itemMatcher,
+                                                 @NonNull final int targetViewId) {
+
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has view id " + itemMatcher + " at position " + position);
+            }
+
+            @Override
+            public boolean matchesSafely(final RecyclerView recyclerView) {
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                View targetView = viewHolder.itemView.findViewById(targetViewId);
+                return itemMatcher.matches(targetView);
+            }
+        };
+    }
+}
